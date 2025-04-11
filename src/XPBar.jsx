87d396420ad9399b93f91539dfb0bff6c6774d29
Eitán Sneider <esneider@Eitans-MacBook-Pro.local>
2025-04-11 00:36:36 -0400

    Make pop-ups narrower for mobile

--- src/XPBar.jsx
+++ src/XPBar.jsx
@@ -4,7 +4,7 @@ const XPBar = ({ xp, maxXp }) => {
   const percentage = Math.min((xp / maxXp) * 100, 100);
 
   return (
-    <div className="w-full bg-gray-300 rounded-xl h-6 overflow-hidden mt-4">
+    <div className="w-full bg-gray-300 rounded-xl h-6 overflow-hidden mt-8">
       <div
         className="bg-green-500 h-full transition-all duration-500"
         style={{ width: `${percentage}%` }}
